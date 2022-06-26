9106213eb2246914481ddd649d8b425de6c5fcd7
mohammad sorooshfar <mohammad.sorooshfar@yahoo.com>
2022-06-26 14:50:09 +0430

    add search page

--- src/components/products.js
+++ src/components/products.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Accordion from "react-bootstrap/Accordion";
 export default function Products(props) {
   const navigate = useNavigate();
   return (
@@ -20,16 +21,14 @@ export default function Products(props) {
                 className="ms-2"
               />
               <h2 className="text-danger ms-3">ترب</h2>
+              <button className="btn btn-danger btn-search" type="button">
+                <i className="fa fa-search"></i>
+              </button>
               <input
                 type="text"
-                className="form-control rounded"
+                className="form-control input-search"
                 placeholder="نام کالا را وارد کنید"
               />
-              <div className="input-group-append">
-                <button className="btn btn-danger" type="button">
-                  <i className="fa fa-search"></i>
-                </button>
-              </div>
             </div>
             <button
               className="btn btn-primary"
@@ -111,6 +110,232 @@ export default function Products(props) {
           </Nav>
         </Container>
       </Navbar>
+      <aside>
+        <Accordion defaultActiveKey={["0", "1"]} alwaysOpen>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>زیر دسته ها</Accordion.Header>
+            <Accordion.Body>
+              <div
+                className="brand-div d-flex justify-content-between align-items-center mb-2"
+                onClick={() => navigate("/mobiles/samsung")}
+              >
+                <p>سامسونگ</p>
+                <p>samsung</p>
+              </div>
+              <div
+                className="brand-div d-flex justify-content-between align-items-center mb-2"
+                onClick={() => navigate("/mobiles/xiaomi")}
+              >
+                <p>شیائومی</p>
+                <p>xiaomi</p>
+              </div>
+              <div
+                className="brand-div d-flex justify-content-between align-items-center mb-2"
+                onClick={() => navigate("/mobiles/apple")}
+              >
+                <p>اپل</p>
+                <p>apple</p>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>قیمت</Accordion.Header>
+            <Accordion.Body>
+              <div className="d-flex flex-column justify-content-center align-items-center">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div class="input-group mb-3 w-40">
+                    <div class="input-group-append">
+                      <span class="input-group-text" id="basic-addon2">
+                        از
+                      </span>
+                    </div>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <div class="input-group mb-3 w-40">
+                    <div class="input-group-append">
+                      <span class="input-group-text" id="basic-addon2">
+                        تا
+                      </span>
+                    </div>
+                    <input type="text" class="form-control" />
+                  </div>
+                </div>
+                <button className="btn btn-primary w-25">اعمال فیلتر</button>
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </aside>
+      <main>
+        <div className="row mb-3">
+          <div className="col-3">
+            <select class="form-select">
+              <option selected>مرتب سازی</option>
+              <option value="1">ارزان ترین</option>
+              <option value="2">گران ترین</option>
+              <option value="3">جدید ترین</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <div className="col-md-3 col-2">
+            <div class="card p-1">
+              <span class="wish-icon me-2">
+                <i class="fa fa-heart-o"></i>
+              </span>
+              <img
+                class="card-img-top"
+                src="https://storage.torob.com/backend-api/base/images/oy/jj/oyjjdjEguJdNpuw2.jpg_/0x145.jpg"
+                alt="Card cap"
+              />
+              <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 class="card-title text-secondary">گوشی موبایل آیفون 13</h5>
+                <p class="card-text">۴۵,۰۰۰,۰۰۰ تومان</p>
+                <a href="#" class="btn btn-outline-success">
+                  مشاهده فروشندگان
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-2">
+            <div class="card p-1">
+              <span class="wish-icon me-2">
+                <i class="fa fa-heart-o"></i>
+              </span>
+              <img
+                class="card-img-top"
+                src="https://storage.torob.com/backend-api/base/images/oy/jj/oyjjdjEguJdNpuw2.jpg_/0x145.jpg"
+                alt="Card cap"
+              />
+              <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 class="card-title text-secondary">گوشی موبایل آیفون 13</h5>
+                <p class="card-text">۴۵,۰۰۰,۰۰۰ تومان</p>
+                <a href="#" class="btn btn-outline-success">
+                  مشاهده فروشندگان
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-2">
+            <div class="card p-1">
+              <span class="wish-icon me-2">
+                <i class="fa fa-heart-o"></i>
+              </span>
+              <img
+                class="card-img-top"
+                src="https://storage.torob.com/backend-api/base/images/oy/jj/oyjjdjEguJdNpuw2.jpg_/0x145.jpg"
+                alt="Card cap"
+              />
+              <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 class="card-title text-secondary">گوشی موبایل آیفون 13</h5>
+                <p class="card-text">۴۵,۰۰۰,۰۰۰ تومان</p>
+                <a href="#" class="btn btn-outline-success">
+                  مشاهده فروشندگان
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-2">
+            <div class="card p-1">
+              <span class="wish-icon me-2">
+                <i class="fa fa-heart-o"></i>
+              </span>
+              <img
+                class="card-img-top"
+                src="https://storage.torob.com/backend-api/base/images/oy/jj/oyjjdjEguJdNpuw2.jpg_/0x145.jpg"
+                alt="Card cap"
+              />
+              <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 class="card-title text-secondary">گوشی موبایل آیفون 13</h5>
+                <p class="card-text">۴۵,۰۰۰,۰۰۰ تومان</p>
+                <a href="#" class="btn btn-outline-success">
+                  مشاهده فروشندگان
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-3 col-2">
+            <div class="card p-1">
+              <span class="wish-icon me-2">
+                <i class="fa fa-heart-o"></i>
+              </span>
+              <img
+                class="card-img-top"
+                src="https://storage.torob.com/backend-api/base/images/oy/jj/oyjjdjEguJdNpuw2.jpg_/0x145.jpg"
+                alt="Card cap"
+              />
+              <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 class="card-title text-secondary">گوشی موبایل آیفون 13</h5>
+                <p class="card-text">۴۵,۰۰۰,۰۰۰ تومان</p>
+                <a href="#" class="btn btn-outline-success">
+                  مشاهده فروشندگان
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-2">
+            <div class="card p-1">
+              <span class="wish-icon me-2">
+                <i class="fa fa-heart-o"></i>
+              </span>
+              <img
+                class="card-img-top"
+                src="https://storage.torob.com/backend-api/base/images/oy/jj/oyjjdjEguJdNpuw2.jpg_/0x145.jpg"
+                alt="Card cap"
+              />
+              <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 class="card-title text-secondary">گوشی موبایل آیفون 13</h5>
+                <p class="card-text">۴۵,۰۰۰,۰۰۰ تومان</p>
+                <a href="#" class="btn btn-outline-success">
+                  مشاهده فروشندگان
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-2">
+            <div class="card p-1">
+              <span class="wish-icon me-2">
+                <i class="fa fa-heart-o"></i>
+              </span>
+              <img
+                class="card-img-top"
+                src="https://storage.torob.com/backend-api/base/images/oy/jj/oyjjdjEguJdNpuw2.jpg_/0x145.jpg"
+                alt="Card cap"
+              />
+              <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 class="card-title text-secondary">گوشی موبایل آیفون 13</h5>
+                <p class="card-text">۴۵,۰۰۰,۰۰۰ تومان</p>
+                <a href="#" class="btn btn-outline-success">
+                  مشاهده فروشندگان
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-2">
+            <div class="card p-1">
+              <span class="wish-icon me-2">
+                <i class="fa fa-heart-o"></i>
+              </span>
+              <img
+                class="card-img-top"
+                src="https://storage.torob.com/backend-api/base/images/oy/jj/oyjjdjEguJdNpuw2.jpg_/0x145.jpg"
+                alt="Card cap"
+              />
+              <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <h5 class="card-title text-secondary">گوشی موبایل آیفون 13</h5>
+                <p class="card-text">۴۵,۰۰۰,۰۰۰ تومان</p>
+                <a href="#" class="btn btn-outline-success">
+                  مشاهده فروشندگان
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
