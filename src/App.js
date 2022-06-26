f09ee12e13d76e57882bcfc513dd0ef87843f8a8
mohammad sorooshfar <mohammad.sorooshfar@yahoo.com>
2022-06-26 09:14:39 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import { Routes, Route } from "react-router-dom";
 import "./components/style.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./components/homePage";
+import Products from "./components/products";
 function App() {
   return (
     <>
       <div>
         <Routes>
           <Route path="/" element={<Home />} exact />
+          <Route path="/mobiles" element={<Products />} exact />
+          <Route path="/tablets" element={<Products />} exact />
+          <Route path="/laptops" element={<Products />} exact />
+          <Route path="/mobiles/samsung" element={<Products />} exact />
+          <Route path="/tablets/samsung" element={<Products />} exact />
+          <Route path="/laptops/lenovo" element={<Products />} exact />
+          <Route path="/mobiles/xiaomi" element={<Products />} exact />
+          <Route path="/tablets/xiaomi" element={<Products />} exact />
+          <Route path="/laptops/asus" element={<Products />} exact />
+          <Route path="/mobiles/apple" element={<Products />} exact />
+          <Route path="/tablets/apple" element={<Products />} exact />
+          <Route path="/laptops/apple" element={<Products />} exact />
         </Routes>
       </div>
     </>
