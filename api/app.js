8562f9cf7d322ac8e97e6f6631c10764bd9da295
miganmht <miganmht@gmail.com>
2022-06-26 19:23:17 +0430

    adding signup

---
+++ api/app.js
@@ -0,0 +1,16 @@
+
+var express = require('express');
+
+var signup=require("./routes/signup")
+
+
+var app = express();
+
+
+app.use(express.json());
+
+
+app.use("/signup",signup);
+
+
+module.exports = app;
