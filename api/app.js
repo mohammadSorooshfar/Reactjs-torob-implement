c154b23bb04cceac86f2f09721ae38c9674fec0d
miganmht <miganmht@gmail.com>
2022-06-26 19:50:51 +0430

    adding sigin and .env to /api for auth

--- api/app.js
+++ api/app.js
@@ -1,7 +1,9 @@
 
 var express = require('express');
 var signup=require("./routes/signup");
+var signin=require("./routes/signin")
 var app = express();
 app.use(express.json());
 app.use("/signup",signup);
+app.use("/signin",signin)
 module.exports = app;
---
+++ api/routes/signin.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var router = express.Router();
+var mysql = require("mysql");
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+// get config vars
+dotenv.config();
+
+// access config var
+process.env.TOKEN_SECRET;
+
+function generateAccessToken(userId) {
+    return jwt.sign(userId, process.env.TOKEN_SECRET, { expiresIn: "1800s" });
+  }
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "torob",
+  });
+con.connect(function (err) {
+    if (err) throw err;
+  });
+router.post("/", function(req, res) {
+    var email=req.body.email;
+    var password=req.body.password;
+    var sql_signin="SELECT * FROM user WHERE email='"+email+"' AND password='"+password+"'";
+    con.query(sql_signin,function(err,result){
+        if(err) throw err;
+        const token = generateAccessToken({ userid: req.body.email });
+        res.status(200).send({
+            token: token,
+            message: "successful",
+          });
+    })
+});
+
+module.exports = router;
