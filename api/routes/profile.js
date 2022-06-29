c6a17b636d0f9c7cb1cda4f905de4e181e8c201e
miganmht <miganmht@gmail.com>
2022-06-29 12:18:35 +0430

    adding product done(no test)

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -70,6 +70,7 @@ router.post("/add_product",authenticateToken,function(req, res){
       var type=req.body.type;
       var date=new Date();
       var time=date.getTime();
+
       if(type="laptop"){
         var cpu=req.body.cpu;
         var gpu=req.body.gpu;
@@ -103,7 +104,7 @@ router.post("/add_product",authenticateToken,function(req, res){
                         con.query(sql5,function(err,result){
                           if(err) throw err;
                           res.status(200).send({
-                            message:"adding product successful",
+                            message:"کالا با موفقیت اضافه شد",
                             code:200
                           })
                         })
@@ -115,7 +116,25 @@ router.post("/add_product",authenticateToken,function(req, res){
                 var sql2="INSERT INTO commodity (name,low_price,high_price,model,type,img_link,time) VALUES ('"+pname+"','"+pprice+"','"+pprice+"','"+model+",'laptop','"+pimglink+"','"+time+"')";
                 con.query(sql2,function(err,result){
                     if(err) throw err;
+                    var sql3="SELECT * FROM commodity WHERE name='"+pname+"'";
+                    con.query(sql3,function(err,result){
+                      if(err) throw err;
+                      let commodityid=result.id;
+                      var sql4="INSERT INTO shop_commodity (commodityid,shopid,price,link) VALUES ('"+commodityid+"','"+shop+"','"+pprice+"','"+plink+"')";
+                      con.query(sql4,function(err,result){
+                        if(err) throw err;
+                        var sql5="INSERT INTO  laptop (commodityid,ram,gpu,cpu,Page_dimensions) VALUES ('"+commodityid+"','"+ram+"','"+gpu+"','"+cpu+"','"+page_dimensions+"')";
+                        con.query(sql5,function(err,result){
+                          if(err) throw err;
+                          res.status(200).send({
+                            message:"کالا با موفقیت اضافه شد",
+                            code:200
+                          })
+                        })
+                      })
+                    })
                     // adding to shop_commodity and laptop table
+                    
                 })
           }
         })
@@ -151,7 +170,7 @@ router.post("/add_product",authenticateToken,function(req, res){
                         con.query(sql5,function(err,result){
                           if(err) throw err;
                           res.status(200).send({
-                            message:"adding product successful",
+                            message:"کالا با موفقیت اضافه شد",
                             code:200
                           })
                         })
@@ -160,7 +179,29 @@ router.post("/add_product",authenticateToken,function(req, res){
               }
             })
           }else{
-            var sql2="INSERT INTO commodity (name,low_price,high_price,model,type,img_link,time) VALUES ('"+pname+"','"+pprice+"','"+pprice+"','"+model+",'mobile','"+pimglink+"','"+time+"')"
+                var sql2="INSERT INTO commodity (name,low_price,high_price,model,type,img_link,time) VALUES ('"+pname+"','"+pprice+"','"+pprice+"','"+model+",'mobile','"+pimglink+"','"+time+"')";
+                con.query(sql2,function(err,result){
+                    if(err) throw err;
+                    var sql3="SELECT * FROM commodity WHERE name='"+pname+"'";
+                    con.query(sql3,function(err,result){
+                      if(err) throw err;
+                      let commodityid=result.id;
+                      var sql4="INSERT INTO shop_commodity (commodityid,shopid,price,link) VALUES ('"+commodityid+"','"+shop+"','"+pprice+"','"+plink+"')";
+                      con.query(sql4,function(err,result){
+                        if(err) throw err;
+                        var sql5="INSERT INTO  mobile (commodityid,ram,color,warranty,weight) VALUES ('"+commodityid+"','"+ram+"','"+color+"','"+warranty+"','"+weight+"')";
+                        con.query(sql5,function(err,result){
+                          if(err) throw err;
+                          res.status(200).send({
+                            message:"کالا با موفقیت اضافه شد",
+                            code:200
+                          })
+                        })
+                      })
+                    })
+                    // adding to shop_commodity and laptop table
+                    
+                })
           }
         })
       }else{
@@ -196,7 +237,7 @@ router.post("/add_product",authenticateToken,function(req, res){
                         con.query(sql5,function(err,result){
                           if(err) throw err;
                           res.status(200).send({
-                            message:"adding product successful",
+                            message:"کالا با موفقیت اضافه شد",
                             code:200
                           })
                         })
@@ -205,7 +246,29 @@ router.post("/add_product",authenticateToken,function(req, res){
               }
             })
           }else{
-            var sql2="INSERT INTO commodity (name,low_price,high_price,model,type,img_link,time) VALUES ('"+pname+"','"+pprice+"','"+pprice+"','"+model+",'tablet','"+pimglink+"','"+time+"')"
+                var sql2="INSERT INTO commodity (name,low_price,high_price,model,type,img_link,time) VALUES ('"+pname+"','"+pprice+"','"+pprice+"','"+model+",'tablet','"+pimglink+"','"+time+"')";
+                con.query(sql2,function(err,result){
+                    if(err) throw err;
+                    var sql3="SELECT * FROM commodity WHERE name='"+pname+"'";
+                    con.query(sql3,function(err,result){
+                      if(err) throw err;
+                      let commodityid=result.id;
+                      var sql4="INSERT INTO shop_commodity (commodityid,shopid,price,link) VALUES ('"+commodityid+"','"+shop+"','"+pprice+"','"+plink+"')";
+                      con.query(sql4,function(err,result){
+                        if(err) throw err;
+                        var sql5="INSERT INTO  mobile (commodityid,ram,color,warranty,weight) VALUES ('"+commodityid+"','"+ram+"','"+color+"','"+warranty+"','"+weight+"')";
+                        con.query(sql5,function(err,result){
+                          if(err) throw err;
+                          res.status(200).send({
+                            message:"کالا با موفقیت اضافه شد",
+                            code:200
+                          })
+                        })
+                      })
+                    })
+                    // adding to shop_commodity and laptop table
+                    
+                })
           }
         })
       }
